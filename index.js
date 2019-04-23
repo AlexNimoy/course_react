b015fccb6696fdf21c400d8ac38d0245f482668d
Александр Поляков <backstabe@gmail.com>
2019-04-23 23:01:04 +0300

    feature: receipt

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Arithmetic from './src/Arithmetic';
 
-class Calculate extends Component {
+import Receipt from '~/src/Receipt';
+
+class ReceiptComponent extends Component {
   constructor(props) {
     super(props);
+    this.state = { positions: [] };
+  }
+
+  componentDidMount() {
+    const positions = [
+      { id: 1, name: 'Product 1', cost: 2.50 },
+      { id: 2, name: 'Product 2', cost: 2.99 },
+      { id: 3, name: 'Product 3', cost: 3.70 }
+    ];
+
+    this.setState({ positions });
   }
 
   render() {
-    const { x, y, operation } = this.props;
+    const { positions } = this.state;
+    const receipt = new Receipt(positions);
 
-    return new Arithmetic(x, y)[operation]() ;
+    return (
+      <table border='1' width='25%'>
+        <thead>
+          <tr>
+            <td width='85%' >Product</td>
+            <td>Price</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <ReceiptPositions
+            positions={ receipt.show() }
+          />
+
+          <Amount amount={ receipt.amount() } />
+          <Total total={ receipt.total() }/>
+        </tbody>
+      </table>
+    );
   }
 }
 
+const Amount = ({ amount }) => (
+  <tr><td>Amount</td><td>{amount}</td></tr>
+);
+
+const Total = ({ total }) => (
+  <tr><td>Total</td><td>{total}</td></tr>
+);
+
+const ReceiptPositions = ({ positions }) => (
+  positions.map((i, index) =>
+    <tr key={ index }>
+      <td>{ i.name }</td>
+      <td>{ i.cost }</td>
+    </tr>
+  )
+);
+
 ReactDOM.render(
-  <Calculate x='4' y='2' operation='add' />,
+  <ReceiptComponent />,
   document.getElementById('root')
 )
