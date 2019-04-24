cd8f556897a19fa61e7cf80d4e90973863c84aa1
Александр Поляков <backstabe@gmail.com>
2019-04-24 16:33:35 +0300

    feature: counter

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-import React, { Children } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const Segment = ({children, dashed, color}) => (
-  <div
-    style={{
-      border: `1px ${dashed ? 'dashed' : 'solid'} ${color}`
-    }}
-  >
-    {Children.map(
-      children,
-      (child, key) => (
-        <div
-          key={key}
-          style={{ border: '1px solid red' }}
-        >
-          {child}
-        </div>
-      )
-    )}
-  </div>
-);
+class Counter extends Component {
+  constructor(props) {
+    super(props);
 
-Segment.defaultProps = {
-  color: 'black'
+    this.state = { value: props.value }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    const delta = e.shiftKey ? 10 : 1;
+    this.setState((prevState) => ({ value: prevState.value + delta }));
+  }
+
+  render() {
+    const { value } = this.state;
+    return(
+      <div>
+        <h1>Counter: { value }</h1>
+        <button
+          onClick={ this.handleClick }
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+Counter.defaultPrors = {
+  value: 0
 }
 
 ReactDOM.render(
-  <Segment>
-    <div>sad</div>
-    <div>sadf</div>
-    {() => <div>Third</div>}
-  </Segment>,
+  <Counter value={1}/>,
   document.getElementById('root')
 )
