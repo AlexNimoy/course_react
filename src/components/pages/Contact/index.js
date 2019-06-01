568f2ec2b1a90706c21e2e780272d644633a31ed
Александр Поляков <backstabe@gmail.com>
2019-06-01 23:13:01 +0300

    feature: formik

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,5 +1,82 @@
 import React from 'react';
+import { withFormik, Form, Field, FieldArray } from 'formik';
 
-const ContactPage = () => <h1>ContactPage</h1>;
+const Error = ({ children }) => {
+  if(children)
+    return(
+      <span style={{ color: 'red' }} >{ children }</span>
+    );
 
-export default ContactPage;
+  return null;
+};
+
+const ContactPage = ({ errors, values }) => (
+  <Form>
+    <div>
+      <Field
+        type='text'
+        name='title'
+        placeholder='title'
+      />
+      <Error>{ errors.title }</Error>
+    </div>
+
+    <div>
+      <h2>Periods</h2>
+      <FieldArray
+        name='periods'
+        render={
+          (arrayHelpers) => (
+            <div>
+              { values.periods && values.periods.length > 0 && values.periods.map(
+                (_, index) => (
+                  <div key={ index }>
+                    <Field type='date' name={`periods[${index}].startFrom`} placeholder='From' />
+                    <Field type='date' name={`periods[${index}].endAt`} placeholder='End' />
+                    <Field type='number' name={`periods[${index}].price`} placeholder='Price' />
+                    <button
+                      type='button'
+                      onClick={()=> arrayHelpers.remove(index)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                )
+              )}
+
+              <button
+                type='button'
+                onClick={() => arrayHelpers.push({
+                  startFrom: '',
+                  endAt: '',
+                  price: 0
+                })}
+              >
+                Add Period
+              </button>
+            </div>
+          )
+        }
+      />
+    </div>
+
+    <button type='submit'>Save</button>
+  </Form>
+);
+
+const ContactPageFormContainer = withFormik({
+  mapPropsToValues: () => ({
+    title: 'New Price List'
+  }),
+  handleSubmit: (values) => alert(JSON.stringify(values)),
+  validate: (values) => {
+    const errors = {};
+
+    if(!values.title)
+      errors.title = 'Required';
+
+    return errors;
+  }
+})(ContactPage);
+
+export default ContactPageFormContainer;
