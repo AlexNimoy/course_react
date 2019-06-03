4d40ab84ab847c5d35e28323ee110cd9c017e9fd
Александр Поляков <backstabe@gmail.com>
2019-06-04 01:08:38 +0300

    feature: checkout form

---
+++ src/components/pages/Cart/CheckoutForm.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { withFormik, Formik, Form, Field } from 'formik';
+
+const Errors = ({ errors, touched, fieldName }) => {
+  if(errors[fieldName] && touched[fieldName]) {
+    return(
+      <div className='error'>
+        { errors[fieldName] }
+      </div>
+    );
+  }
+
+  return null;
+}
+
+const CheckoutFrom = props => {
+  const {
+    values,
+    touched,
+    errors,
+    handleBlur,
+    handleChange,
+    handleSubmit
+  } = props;
+
+  return(
+    <div className='checkout_form'>
+      <h1>Checkout</h1>
+      <Formik
+        render={() => (
+          <Form onSubmit={  handleSubmit  }>
+            <Field
+              name='fio'
+              onChange={ handleChange }
+              onBlur={ handleBlur }
+              placeholder='ФИО'
+            />
+            <Errors {...props} fieldName='fio'/>
+
+            <Field
+              name='phone'
+              onChange={ handleChange }
+              onBlur={ handleBlur }
+              placeholder='Телефон'
+            />
+            <Errors {...props} fieldName='phone'/>
+
+            <Field
+              name='email'
+              onChange={ handleChange }
+              onBlur={ handleBlur }
+              placeholder='Email'
+            />
+            <Errors {...props} fieldName='email'/>
+
+            <Field
+              name='address'
+              onChange={ handleChange }
+              onBlur={ handleBlur }
+              placeholder='Адрес'
+            />
+            <Errors {...props} fieldName='address'/>
+
+            <button type='submit'>Submit</button>
+          </Form>
+        )}
+      />
+    </div>
+  );
+}
+
+const ContactPageFormContainer = withFormik({
+  handleSubmit: (values) => alert(JSON.stringify(values)),
+  validate: values => {
+    const errors = {};
+
+    if (!values.fio) {
+      errors.fio = 'Required';
+    }
+
+    if (!values.phone) {
+      errors.phone = 'Required';
+    }
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.address) {
+      errors.address = 'Required';
+    }
+
+    return errors;
+  }
+})(CheckoutFrom);
+
+export default ContactPageFormContainer;
--- src/components/pages/Cart/widgets/Products/Products.js
+++ src/components/pages/Cart/widgets/Products/Products.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Product from './Product';
+import CheckoutForm from '../../CheckoutForm';
+
 const Table = props => (
-  <table className='cart__reciept' >
-    <thead>
-      <tr>
-        <th>Product</th>
-        <th>quantity</th>
-        <th>price</th>
-      </tr>
-    </thead>
-    <tbody>
-      { props.cartProducts.map((item, key) =>
-          <Product {...item} key={key} />
-      )}
-    </tbody>
-  </table>
+  <Fragment>
+    <table className='cart__reciept' >
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>quantity</th>
+          <th>price</th>
+        </tr>
+      </thead>
+      <tbody>
+        { props.cartProducts.map((item, key) =>
+            <Product {...item} key={key} />
+        )}
+      </tbody>
+    </table>
+    <CheckoutForm cart={ props.cart }/>
+  </Fragment>
 )
 
 Table.propTypes = {
