079d9dec98b774fe04675cc6b943632488845c63
Александр Поляков <backstabe@gmail.com>
2019-07-04 00:32:37 +0300

    featue: move up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from 'components/App';
+import history from 'helpers/history';
+import historyCb from 'helpers/historyCb';
+import store from 'store';
 
-import 'css/styles.css';
-import 'css/form.css';
+history.listen(historyCb);
+historyCb(window.location);
 
 ReactDOM.render(
-  <App />,
+  <App history={ history } store={ store } />,
   document.getElementById('root')
 )
